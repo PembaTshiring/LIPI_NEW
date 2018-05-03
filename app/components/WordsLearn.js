cf41dc19d2b624042dc4bbd001553b7f74d25028
Binayamagar <binayamagar29@gmail.com>
2018-05-03 13:17:50 +0545

    added wordslearn file

---
+++ app/components/WordsLearn.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, StatusBar, TouchableOpacity, ImageBackground } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+export default class App extends React.Component {
+  //hiding statusBar
+componentDidMount() {
+  StatusBar.setHidden(true);
+  // this.setState({isHidden: false})
+}
+  render() {
+    return (
+
+      <View style={styles.container}>        
+      <ImageBackground source={require('./app/assets/images/background/bg3.jpg')} style={styles.bg}>
+        <View style={styles.top}>
+          <View style={styles.setting}>
+          <Ionicons name='md-settings' size={33} color="#fff" />
+        </View>
+      </View>
+        <View style={styles.center}>
+          <View style={styles.menu1}>
+            <View style={styles.menuInner}>
+            <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+            <Image style={styles.level}
+          source={require('./app/assets/images/buttons/names.png')}
+            />
+            </TouchableOpacity>
+        </View>
+          </View>
+        <View style={styles.menu1}>
+            <View style={styles.menuInner}>
+            <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+            <Image style={styles.level}
+          source={require('./app/assets/images/buttons/days.png')}
+            />
+          </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.menu1}>
+            <View style={styles.menuInner}>
+            <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+            <Image style={styles.level}
+          source={require('./app/assets/images/buttons/colors.png')}
+            />
+            </TouchableOpacity>
+        </View>
+          </View>
+        <View style={styles.menu1}>
+            <View style={styles.menuInner}>
+            <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+       >
+            <Image style={styles.level}
+          source={require('./app/assets/images/buttons/persons.png')}
+            />
+          </TouchableOpacity>
+          </View>
+        </View>
+
+  
+  
+          
+        </View>
+        </ImageBackground>
+      </View>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+},
+  container: {
+    flex: 1,
+  },
+  top: {
+    height: '15%',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  setting:{
+    height: 30,
+    width:30,
+    marginRight: 15,
+    alignSelf: 'flex-end', 
+  },
+  center: {
+    height: '85%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 5,
+    justifyContent:'center',
+  },
+  menu1:{
+    height: '45%',
+    width:'45%',
+    padding: 5,    
+  },
+  menuInner:{
+    flex: 1,
+  },
+  level:{
+    width: '100%',
+    height: '100%',
+    resizeMode: Image.resizeMode.contain,
+  },
+  
+  
+});
