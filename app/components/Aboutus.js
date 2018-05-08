7bc271d0b4f622afc2f71812ce27d6c33e357da7
DeXteR25 <sujanbantawa25@gmail.com>
2018-05-08 13:13:22 +0545

    about us

---
+++ app/components/Aboutus.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import { Text, 
+    View,
+    StatusBar,
+    TouchableOpacity,
+    StyleSheet,    
+    Dimensions,
+    Image,
+   
+} from 'react-native'
+import { Ionicons,FontAwesome } from '@expo/vector-icons';
+const { width, height } = Dimensions.get("window");
+
+export default class Aboutus extends Component {
+    
+    constructor () {
+        super() 
+    }
+    componentDidMount() {
+        StatusBar.setHidden(true);
+        
+}
+    static navigationOptions = {
+        headerVisible: false,
+        header: null
+    };
+    render() {
+     
+    return (
+     
+      <View style={styles.container}>
+      <View style={styles.top}>
+      <Ionicons name="md-arrow-back" size={33} />
+      </View>
+          <View style={styles.header}>
+            <Text style={styles.text}>LIPI APP </Text>
+          </View>
+          <View style={styles.paragraph}>
+          <Text style={{justifyContent:'center'}}>Lipi App is fully developed in React Native by Nevolution Tech pvt ltd. It is learning app which is specially targeted to small kids. Lipi App is underdevelop app new features will be added in future. </Text>
+          <Text>-------------------------Ver:1.04-----------------------</Text>
+     </View>
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexWrap: "wrap",
+      alignItems: "center",
+    
+      
+    },
+    header: {
+      height:'20%',
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    
+    paragraph: {
+      width:'60%',
+      alignContent: 'center',
+      justifyContent: 'center',
+      
+    },
+    text:{
+      fontSize: 30,
+    },
+    top:{
+      height: '15%',
+      width: '100%',
+      justifyContent:'center',
+      marginLeft: '10%',
+    },
+    
+  });
